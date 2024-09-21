609f90f8d04b623703096ca8f95e5effebd80270
Ryang Heo <117929916+ryang1119@users.noreply.github.com>
2024-09-22 00:39:40 +0900

    Update run_sft.sh

--- ATOSS/src/run_sft.sh
+++ ATOSS/src/run_sft.sh
@@ -13,7 +13,7 @@ python SFT.py --task $TASK \
             --gradient_accumulation_steps 1 \
             --eval_batch_size 16 \
             --learning_rate 6e-5 \
-            --num_train_epochs 1 \
+            --num_train_epochs 50 \
             --max_seq_length 180 \
             --check_val_every_n_epoch 1 \
             --save_top_k 1
